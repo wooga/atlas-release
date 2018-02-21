9252b74fe6bae70b2c6ac96db3f784cfa3507828
Manfred Endres <Larusso@users.noreply.github.com>
2018-02-21 15:51:22 +0100

    Restructure package layout and add groovy docs (#35)
    * Restructure package layout and add groovy docs
    
    Description
    ===========
    This pullrequst update the package layout to have he same structual
    layout as all other `wooga` plugins. It also adds groovy documentation
    strings.
    
    Changes
    =======
    
    ![CHANGE] package structure
    ![ADD] groovydoc comments to public classes and methods
    
    * Fix broken tests
    
    The test for versionCode generation was broken for a long time.
    We changed the default release scope from minor to patch.

--- src/main/groovy/wooga/gradle/release/AtlasReleasePluginExtension.groovy
+++ src/main/groovy/wooga/gradle/release/AtlasReleasePluginExtension.groovy
@@ -9,34 +9,25 @@ import org.gradle.util.ConfigureUtil
  * Extension object for <code>net.wooga.release</code> plugin.
  * This extension allows to set further default configurations for the plugin.
  */
-class AtlasReleasePluginExtension {
-
-    private PatternSet metaCleanPattern = new PatternSet()
-
+interface AtlasReleasePluginExtension {
     /**
      *
      * @return a pattern
      * @see org.gradle.api.tasks.util.PatternFilterable
      */
-    PatternFilterable getMetaCleanPattern() {
-        this.metaCleanPattern
-    }
+    PatternFilterable getMetaCleanPattern()
 
     /**
      * Allows to configure the <code>metaCleanPattern</code> filter object with an <code>Action</code> object.
      * @param action an action object to use for configuring the <code>metaCleanPattern</code>
      * @see #metaCleanPattern
      */
-    void metaCleanPattern(Action<PatternFilterable> action) {
-        action.execute(this.metaCleanPattern)
-    }
+    void metaCleanPattern(Action<PatternFilterable> action)
 
     /**
      * Allows to configure the <code>metaCleanPattern</code> filter object with a <code>Closure</code> object.
      * @param configureClosure a closure object to use for configuring the <code>metaCleanPattern</code>
      * @see #metaCleanPattern
      */
-    void metaCleanPattern(Closure configureClosure) {
-        metaCleanPattern(ConfigureUtil.configureUsing(configureClosure))
-    }
+    void metaCleanPattern(Closure configureClosure)
 }
---
+++ src/main/groovy/wooga/gradle/release/internal/DefaultAtlasReleasePluginExtension.groovy
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package wooga.gradle.release.internal
+
+import org.gradle.api.Action
+import org.gradle.api.tasks.util.PatternFilterable
+import org.gradle.api.tasks.util.PatternSet
+import org.gradle.util.ConfigureUtil
+import wooga.gradle.release.AtlasReleasePluginExtension
+
+class DefaultAtlasReleasePluginExtension implements AtlasReleasePluginExtension{
+    private PatternSet metaCleanPattern = new PatternSet()
+
+    /**
+     *
+     * @return a pattern
+     * @see org.gradle.api.tasks.util.PatternFilterable
+     */
+    PatternFilterable getMetaCleanPattern() {
+        this.metaCleanPattern
+    }
+
+    /**
+     * Allows to configure the <code>metaCleanPattern</code> filter object with an <code>Action</code> object.
+     * @param action an action object to use for configuring the <code>metaCleanPattern</code>
+     * @see #metaCleanPattern
+     */
+    void metaCleanPattern(Action<PatternFilterable> action) {
+        action.execute(this.metaCleanPattern)
+    }
+
+    /**
+     * Allows to configure the <code>metaCleanPattern</code> filter object with a <code>Closure</code> object.
+     * @param configureClosure a closure object to use for configuring the <code>metaCleanPattern</code>
+     * @see #metaCleanPattern
+     */
+    void metaCleanPattern(Closure configureClosure) {
+        metaCleanPattern(ConfigureUtil.configureUsing(configureClosure))
+    }
+}
