393c6bbf24a24576883d54a07d4965d8f42e292a
Manfred Endres <manfred.endres@tslarusso.de>
2018-08-13 16:29:21 +0200

    Add groovy doc strings for new VersionStrategy configs

--- src/main/groovy/wooga/gradle/release/version/semver2/VersionStrategies.groovy
+++ src/main/groovy/wooga/gradle/release/version/semver2/VersionStrategies.groovy
@@ -86,23 +86,119 @@ final class VersionStrategies {
             enforcePrecedence: true
     )
 
+    /**
+     * Returns a version strategy to be used for {@code final} builds.
+     * <p>
+     * This strategy infers the release version by checking the nearest release.
+     * <p>
+     * Example:
+     * <pre>
+     * {@code
+     * releaseScope = "minor"
+     * nearestVersion = "1.3.0"
+     * inferred = "1.4.0"
+     * }
+     * </pre>
+     */
     static final SemVerStrategy FINAL = DEFAULT.copyWith(
             name: 'production',
 
             stages: ['final','production'] as SortedSet
     )
 
-
+    /**
+     * Returns a version strategy to be used for {@code pre-release}/{@code candidate} builds.
+     * <p>
+     * This strategy infers the release version by checking the nearest any release.
+     * If a {@code pre-release} with the same {@code major}.{@code minor}.{@code patch} version exists, bumps the count part.
+     * <p>
+     * Example <i>new pre release</i>:
+     * <pre>
+     * {@code
+     * releaseScope = "minor"
+     * nearestVersion = "1.2.0"
+     * nearestAnyVersion = ""
+     * inferred = "1.3.0-rc.1"
+     * }
+     * </pre>
+     * <p>
+     * Example <i>pre release version exists</i>:
+     * <pre>
+     * {@code
+     * releaseScope = "minor"
+     * nearestVersion = "1.2.0"
+     * nearestAnyVersion = "1.3.0-rc.1"
+     * inferred = "1.3.0-rc.2"
+     * }
+     * </pre>
+     * <p>
+     * Example <i>last final release higher than pre-release</i>:
+     * <pre>
+     * {@code
+     * releaseScope = "minor"
+     * nearestVersion = "1.3.0"
+     * nearestAnyVersion = "1.3.0-rc.1"
+     * inferred = "1.4.0-rc.1"
+     * }
+     * </pre>
+     */
     static final SemVerStrategy PRE_RELEASE = DEFAULT.copyWith(
             name: 'pre-release',
             stages: ['rc', 'staging'] as SortedSet,
             preReleaseStrategy: all(Strategies.PreRelease.STAGE_FIXED, Strategies.PreRelease.COUNT_INCREMENTED)
     )
 
+    /**
+     * Returns a version strategy to be used for {@code development} builds.
+     * <p>
+     * This strategy creates a unique version string based on last commit hash and the
+     * distance of commits to nearest normal version.
+     * This version string is not compatible with <a href="https://fsprojects.github.io/Paket/">Paket</a> or
+     * <a href="https://www.nuget.org/">Nuget</a>
+     * <p>
+     * Example:
+     * <pre>
+     * {@code
+     * releaseScope = "minor"
+     * nearestVersion = "1.3.0"
+     * commitHash = "90c90b9"
+     * distance = 22
+     * inferred = "1.3.0-dev.22+90c90b9"
+     * }
+     * </pre>
+     */
     static final SemVerStrategy DEVELOPMENT = Strategies.DEVELOPMENT.copyWith(
             normalStrategy: scopes,
             buildMetadataStrategy: NetflixOssStrategies.BuildMetadata.DEVELOPMENT_METADATA_STRATEGY)
 
+    /**
+     * Returns a version strategy to be used for {@code snapshot} builds.
+     * <p>
+     * This strategy creates a snapshot version based on <a href="https://semver.org/spec/v2.0.0.html">Semver 2.0.0</a>.
+     * Branch names will be encoded in the pre-release part of the version.
+     * <p>
+     * Example <i>from master branch</i>:
+     * <pre>
+     * {@code
+     * releaseScope = "minor"
+     * nearestVersion = "1.3.0"
+     * branch = "master"
+     * distance = 22
+     * inferred = "1.4.0-master.22"
+     * }
+     * </pre>
+     * <p>
+     * Example <i>from topic branch</i>:
+     * <pre>
+     * {@code
+     * releaseScope = "minor"
+     * nearestVersion = "1.3.0"
+     * branch = "feature/fix_22"
+     * distance = 34
+     * inferred = "1.4.0-branch.feature.fix.22.34"
+     * }
+     * </pre>
+     */
     static final SemVerStrategy SNAPSHOT = DEFAULT.copyWith(
             name: 'ci',
             stages: ['ci', 'snapshot'] as SortedSet,
